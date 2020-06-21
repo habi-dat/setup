c97245930c544b58c3556db44e12e6cb09d42af9
Florian Humer <hf@muehlboeck.com>
2020-06-21 15:01:00 +0200

    update direktkredit module to new version (major changes

--- direktkredit/setup.sh
+++ direktkredit/setup.sh
@@ -9,27 +9,9 @@ git clone https://github.com/soudis/habidat-direktkredit-platform.git ../store/d
 envsubst < config/settings.env > ../store/direktkredit/settings.env
 envsubst < docker-compose.yml > ../store/direktkredit/docker-compose.yml
 
-if [ $HABIDAT_CREATE_SELFSIGNED == "true" ]
-then
-#	openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \
-#    -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=$HABIDAT_NEXTCLOUD_SUBDOMAIN.$HABIDAT_DOMAIN" \
-#    -keyout "../store/nginx/certificates/$HABIDAT_NEXTCLOUD_SUBDOMAIN.$HABIDAT_DOMAIN.key"  -out "../store/nginx/certificates/$HABIDAT_NEXTCLOUD_SUBDOMAIN.$HABIDAT_DOMAIN.crt"
-
-#    echo "CERT_NAME=$HABIDAT_NEXTCLOUD_SUBDOMAIN.$HABIDAT_DOMAIN" >> ../store/nextcloud/nextcloud.env
-    echo "CERT_NAME=$HABIDAT_DOMAIN" >> ../store/direktkredit/settings.env
-fi
-
 echo "Spinning up containers..."
 
 docker-compose -f ../store/direktkredit/docker-compose.yml -p "$HABIDAT_DOCKER_PREFIX-direktkredit" up -d
-
-
-echo "Add project logo..."
-
-if [ -f "../$HABIDAT_LOGO" ]
-then
-	docker cp "../$HABIDAT_LOGO" $HABIDAT_DOCKER_PREFIX-direktkredit:/habidat/public/images
-fi
 
 # update nextcloud external sites
 echo "Add link to nextcloud..."
