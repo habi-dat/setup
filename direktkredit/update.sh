b915f0503f69db2075660a19d652648a0a158430
Florian Humer <hf@muehlboeck.com>
2020-06-21 14:55:58 +0200

    update direktkredit module to new version (major changes

--- direktkredit/update.sh
+++ direktkredit/update.sh
@@ -10,6 +10,7 @@ envsubst < docker-compose.yml > ../store/direktkredit/docker-compose.yml
 echo "Pulling images and recreate containers (for all projects)..."
 
 cd ../store/direktkredit
+git pull
 ./update-projects.sh all
 ./update-projects.sh nginx
 cd ../../direktkredit
