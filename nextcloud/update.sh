448c09a1f2bd9ba91ba2ecf9b2b227058643304a
Florian Humer <hf@muehlboeck.com>
2020-06-23 14:36:46 +0200

    adding import scripts and fixing up discourse

--- nextcloud/update.sh
+++ nextcloud/update.sh
@@ -24,7 +24,24 @@ echo "Configuring user module..."
 sed -i '/HABIDAT_USER_NEXTCLOUD_DB_PASSWORD=/d' ../store/auth/user.env
 sed -i '/HABIDAT_USER_NEXTCLOUD_API_URL=/d' ../store/auth/user.env
 
+rawurlencode() {
+  local string="${1}"
+  local strlen=${#string}
+  local encoded=""
+  local pos c o
+
+  for (( pos=0 ; pos<strlen ; pos++ )); do
+     c=${string:$pos:1}
+     case "$c" in
+        [-_.~a-zA-Z0-9] ) o="${c}" ;;
+        * )               printf -v o '%%%02x' "'$c"
+     esac
+     encoded+="${o}"
+  done
+  echo "${encoded}"    # You can either set a return variable (FASTER) 
+}
+
 # rewrite API vars to user module env
 echo "HABIDAT_USER_NEXTCLOUD_DB_PASSWORD=$HABIDAT_NEXTCLOUD_DB_PASSWORD" >> ../store/auth/user.env
-echo "HABIDAT_USER_NEXTCLOUD_API_URL=http://admin:$HABIDAT_ADMIN_PASSWORD@$HABIDAT_DOCKER_PREFIX-nextcloud/ocs/v1.php" >> ../store/auth/user.env
+echo "HABIDAT_USER_NEXTCLOUD_API_URL=http://admin:$(rawurlencode $HABIDAT_ADMIN_PASSWORD)@$HABIDAT_DOCKER_PREFIX-nextcloud/ocs/v1.php" >> ../store/auth/user.env
 docker-compose -f ../store/auth/docker-compose.yml -p "$HABIDAT_DOCKER_PREFIX-auth" up -d user
