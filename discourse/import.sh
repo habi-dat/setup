4e181da39ba677aa338ee618469e388984df3ecc
Florian Humer <hf@muehlboeck.com>
2020-06-23 15:53:17 +0200

    adding import scripts and fixing up discourse

--- discourse/import.sh
+++ discourse/import.sh
@@ -3,6 +3,8 @@ set +x
 
 echo "Copying backup file to discourse container..."
 
+docker exec $HABIDAT_DOCKER_PREFIX-discourse mkdir /shared/backups/default
+
 docker cp $HABIDAT_BACKUP_DIR/$HABIDAT_DOCKER_PREFIX/discourse/$1 $HABIDAT_DOCKER_PREFIX-discourse:/shared/backups/default
 
 echo "Enabling restore mode..."
@@ -14,6 +16,7 @@ echo "Restoring..."
 ../store/discourse/launcher run $HABIDAT_DOCKER_PREFIX-discourse "discourse restore $1" > import.log
 
 if grep -q "\[SUCCESS\]" import.log 
+then
 	rm import.log
 	echo "Finished, imported: $1"
 else
