04d36eaff0dfa702985d0705ff9701593b73e986
Florian Humer <hf@muehlboeck.com>
2020-06-23 14:04:14 +0200

    adding import scripts and fixing up discourse

--- nextcloud/import.sh
+++ nextcloud/import.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set +x
 
+
+
 #export HABIDAT_LDAP_BASE=dc=habidat-staging
 #export HABIDAT_LDAP_ADMIN_PASSWORD=A5AFsfDrsr4DYswQ
 
@@ -19,6 +21,8 @@ echo "Extracting data..."
 tar -xzf $HABIDAT_BACKUP_DIR/$HABIDAT_DOCKER_PREFIX/nextcloud/$1 -C $datapath
 
 echo "Restoring database dump..."
+
+source ../store/nextcloud/passwords.env
 
 cat "$datapath/db.sql" | docker exec -i $HABIDAT_DOCKER_PREFIX-nextcloud-db mysql nextcloud -u root --password=$HABIDAT_NEXTCLOUD_DB_ROOT_PASSWORD
 rm "$datapath/db.sql";
