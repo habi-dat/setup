a49a9de69d2d24dc72275bdd152733e3f0e222c7
Florian Humer <hf@muehlboeck.com>
2020-06-23 13:19:06 +0200

    adding import scripts and fixing up discourse

--- auth/import.sh
+++ auth/import.sh
@@ -5,7 +5,7 @@ echo "NOTE: importing data only works for data with the same domain / LDAP base"
 
 echo "Extracting data..."
 
-tar -xzf -C $HABIDAT_BACKUP_DIR/$HABIDAT_DOCKER_PREFIX/auth $1
+tar -xzf -C $HABIDAT_BACKUP_DIR/$HABIDAT_DOCKER_PREFIX/auth $HABIDAT_BACKUP_DIR/$HABIDAT_DOCKER_PREFIX/auth/$1
 
 echo "Restoring user module activation store..."
 docker cp $HABIDAT_BACKUP_DIR/$HABIDAT_DOCKER_PREFIX/auth/activationStore.json "$HABIDAT_DOCKER_PREFIX-user":/habidat-user/data/activationStore.json
