1c24ffc978b9aa56f62c8a7a56dd498df8deda21
Florian Humer <florian.humer@gmail.com>
2020-11-27 11:57:48 +0100

    fix docker-compose for mediawiki

--- mediawiki/remove.sh
+++ mediawiki/remove.sh
@@ -6,6 +6,7 @@ then
 	echo "Destroying containers and volumes for $1 instance..."
 
     docker-compose -f ../store/mediawiki/$1/docker-compose.yml -p "$HABIDAT_DOCKER_PREFIX-mediawiki-$1" down -v --remove-orphans
+    rm -r -r ../store/mediawiki/$1
 
 else
 	echo "Destroying containers and volumes for all instances..."
@@ -16,6 +17,7 @@ else
 	    id=${dir##*/}
 
 	    docker-compose -f ../store/mediawiki/$id/docker-compose.yml -p "$HABIDAT_DOCKER_PREFIX-mediawiki-$id" down -v --remove-orphans
+	    rm -r -r ../store/mediawiki/$id
 	done
 
 	rm -r ../store/mediawiki
