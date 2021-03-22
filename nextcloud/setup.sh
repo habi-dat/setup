51f1a15bcb46f71143ae2dbe86e02d592b455223
Florian Humer <florian.humer@gmail.com>
2021-03-22 08:00:36 +0100

    update

--- nextcloud/setup.sh
+++ nextcloud/setup.sh
@@ -77,8 +77,10 @@ echo "Configuring user module..."
 # remove nextcloud vars from user module env
 sed -i '/HABIDAT_USER_NEXTCLOUD_DB_PASSWORD=/d' ../store/auth/user.env
 sed -i '/HABIDAT_USER_NEXTCLOUD_API_URL=/d' ../store/auth/user.env
+sed -i '/HABIDAT_DISCOURSE_SSO_SECRET=/d' ../store/auth/user.env
 
 # rewrite API vars to user module env
 echo "HABIDAT_USER_NEXTCLOUD_DB_PASSWORD=$HABIDAT_NEXTCLOUD_DB_PASSWORD" >> ../store/auth/user.env
 echo "HABIDAT_USER_NEXTCLOUD_API_URL=http://admin:$HABIDAT_ADMIN_PASSWORD@$HABIDAT_DOCKER_PREFIX-nextcloud/ocs/v1.php" >> ../store/auth/user.env
+echo "HABIDAT_DISCOURSE_SSO_SECRET=$HABIDAT_DISCOURSE_SSO_SECRET" >> ../store/auth/user.env
 docker-compose -f ../store/auth/docker-compose.yml -p "$HABIDAT_DOCKER_PREFIX-auth" up -d user
