1dd9c09d88f44957972c5d89c3a9c73f6155fa09
Florian Humer <florian.humer@gmail.com>
2022-03-13 17:28:12 +0100

    update subgroup functions and update nextcloud to 22.2.5

--- mediawiki/start.sh
+++ mediawiki/start.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-echo "no action for this modules"
+#!/bin/bash
+
+if [[ $# -eq 1 ]] 
+then
+	echo "Starting mediawiki $1 instance..."
+
+    docker-compose -f ../store/mediawiki/$1/docker-compose.yml -p "$HABIDAT_DOCKER_PREFIX-mediawiki-$1" start
+
+else
+	echo "Starting all mediawiki instances..."
+
+	for dir in ../store/mediawiki/*/  # list directories 
+	do
+	    dir=${dir%*/}      # remove the trailing "/"    
+	    id=${dir##*/}
+
+		echo "Updating mediawiki $id instance..."
+	    docker-compose -f ../store/mediawiki/$id/docker-compose.yml -p "$HABIDAT_DOCKER_PREFIX-mediawiki-$id" start
+	done
+fi
