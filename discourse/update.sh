13f60c840c54212e626d77495543940b89ba5800
Florian Humer <florian.humer@gmail.com>
2023-07-27 14:31:25 +0200

    fix discourse update

--- discourse/update.sh
+++ discourse/update.sh
@@ -8,10 +8,14 @@ source ../store/discourse/passwords.env
 
 echo "Rebuilding and starting containers..."
 
-
 envsubst < config/discourse-settings-update.yml > ../store/discourse/bootstrap/discourse-settings.yml
 
-../store/discourse/launcher rebuild $HABIDAT_DOCKER_PREFIX-discourse-data
-../store/discourse/launcher rebuild $HABIDAT_DOCKER_PREFIX-discourse
+cd ../store/discourse
+git pull
+
+./launcher rebuild $HABIDAT_DOCKER_PREFIX-discourse-data
+./launcher rebuild $HABIDAT_DOCKER_PREFIX-discourse
+
+cd ../../discourse
 
 docker network connect $HABIDAT_PROXY_NETWORK $HABIDAT_DOCKER_PREFIX-discourse
